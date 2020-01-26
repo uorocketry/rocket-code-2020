ce0fa4956f148d91e0fdeb9e8e4fb87f11f8d38e
jonl40 <jonathanluu122@gmail.com>
2020-01-26 14:25:52 -0500

    UART works. Add error handling later though

--- Radio_UART_test.c
+++ Radio_UART_test.c
@@ -13,6 +13,8 @@ This command will output “Hello” string on UART port i.e. Tx pin and will di
 By default, mini UART is mapped to the GPIO14 (TX) and GPIO15(RX). While PL011 i.e. ttyAMA0 is connected to the on-board Bluetooth module.
 */
 
+// connect logic analyzer to TX wpi 15 and ground 
+
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
@@ -22,31 +24,34 @@ By default, mini UART is mapped to the GPIO14 (TX) and GPIO15(RX). While PL011 i
 
 int main ()
 {
-  int serial_port ;
+  int serial_port;
   char dat;
   if ((serial_port = serialOpen ("/dev/ttyS0", 9600)) < 0)	/* open serial port */
   {
+    printf("Can't open serial port!!!\n");
     fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
     return 1 ;
   }
 
+  // use the wiringPi pin numbering scheme
   if (wiringPiSetup () == -1)					/* initializes wiringPi setup */
   {
+    printf("Can't initialize wiringPi!!!\n");
     fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
     return 1 ;
   }
 
   while(1){
 
-	if(serialDataAvail (serial_port) )
-	{
-		printf("Transmitting\n");
-		dat = serialGetchar (serial_port);		/* receive character serially*/
+	//if(serialDataAvail (serial_port) )
+	//{
+		printf("\nTransmitting\n");
+		dat = 'A';		/* receive character serially*/
 		printf ("%c", dat) ;
 		fflush (stdout) ;
 		serialPutchar(serial_port, dat);		/* transmit character serially on port */
-		  }
-		printf("Not Transmitting!!!!\n");
+		  //}
+		//printf("Not Transmitting!!!!\n");
 	}
 
 }
