f92e858f2100e77f1ce9a9ec2673cac2ea5575b4
Sebastien Girard <sebastiengrd@gmail.com>
2020-02-15 13:42:47 -0500

    Made the Main program and removed CMake files and added gitignore

---
+++ sbgECom/examples/ellipseMinimal/src/Main.cpp
@@ -0,0 +1,21 @@
+#include "stateMachine/stdafx.h"
+#include "RocketSM.h"
+
+
+#include<thread>
+#include<chrono>
+#include<unordered_map>
+#include "Interface.h"
+
+
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+
+
+
+	return 0;
+}
