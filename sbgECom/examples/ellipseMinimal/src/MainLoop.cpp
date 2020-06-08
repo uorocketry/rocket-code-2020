34d53a148a96566ff6491eddcadf503f05efb47f
Sebastien Girard <sebastiengrd@gmail.com>
2020-06-08 17:18:39 -0400

    added chrono to MainLoop and printed time in milliseconds

--- sbgECom/examples/ellipseMinimal/src/MainLoop.cpp
+++ sbgECom/examples/ellipseMinimal/src/MainLoop.cpp
@@ -1,13 +1,28 @@
 #include "Rocket.h"
+#include "chrono"
+#include "iostream"
+
+#define DELAY 0.005;
+
 
 int main()
 {
 	// create a rocket instance
 	Rocket uOttRocket;
 	
-	while (true) {
+	std::chrono::time_point<std::chrono::high_resolution_clock> start, now; 
+	std::chrono::milliseconds ms;
 
-		uOttRocket.updateRocket();
+	start = std::chrono::system_clock::now();
+
+	
+	while (true) {
+		now = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> elapsed_seconds = now - start;
+		ms = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_seconds);
+		std::cout << ms.count() << "\n";
+
+		// uOttRocket.updateRocket();
 	}
 
 
