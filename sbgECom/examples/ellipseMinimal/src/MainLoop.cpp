d05310022b98c3f9b861e96fef404bc4ca4abb26
Sebastien Girard <sebastiengrd@gmail.com>
2020-06-21 16:13:44 -0400

    execute rocket update at a frequency of 200Hz

--- sbgECom/examples/ellipseMinimal/src/MainLoop.cpp
+++ sbgECom/examples/ellipseMinimal/src/MainLoop.cpp
@@ -3,38 +3,39 @@
 #include "iostream"
 #include <thread>
 
-#define DELAYMS 1000000000 //5000 microseconds = 0.005s = 200 Hz
+#define TARGET_DELAY_NS 1000000000/200 // in nanoseconds = 5 miliseconds = 200Hz
 
 
 int main()
 {
 	// create a rocket instance
-	//Rocket uOttRocket;
-	
-	std::cout << "ok";
+	Rocket uOttRocket;
+
 	std::chrono::time_point<std::chrono::high_resolution_clock> start, now; 
+	std::chrono::duration<int64_t, std::nano> target_ns, elapsed_ns;
 	start = std::chrono::high_resolution_clock::now();
 	
 	unsigned long int count = 1;
 	while (true) {
-		// uOttRocket.updateRocket();
-		
 		now = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<int64_t, std::nano> elapsed_seconds = now - start;
-		
-		std::chrono::microseconds elapsed_seconds_ms = std::chrono::duration_cast<std::chrono::microseconds>(elapsed_seconds);
-		std::chrono::duration<int64_t, std::nano> target_ms(DELAYMS * count++);
-		//https://www.geeksforgeeks.org/chrono-in-c/
-		
-		
-		std::cout << elapsed_seconds.count() << " : ";
-		// std::cout << ms.count() << "\n";
 
-		std::this_thread::sleep_for(target_ms - elapsed_seconds_ms);
+		uOttRocket.updateRocket();
 
-		// std::this_thread::sleep( boost::posix_time::milliseconds(1000) );
+
+		elapsed_ns = std::chrono::duration<int64_t, std::nano>(now - start);
+		target_ns = std::chrono::duration<int64_t, std::nano>(TARGET_DELAY_NS * count++);		
 		
-		
+		// std::cout << "elapsed " << elapsed_ns.count();
+		// std::cout << " Target " << (target_ns).count() << "\n";
+
+
+		if (target_ns > elapsed_ns) {
+			std::this_thread::sleep_for(target_ns - elapsed_ns);
+		} else {
+			std::cout << "ERROR\n";
+		}
+
+		// std::cout.flush();
 	}
 
 
