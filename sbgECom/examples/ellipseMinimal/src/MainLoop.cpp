016733c966d2410133f9eaea335dc9e55bdb0f36
Sebastien Girard <sebastiengrd@gmail.com>
2020-06-21 15:09:35 -0400

    test do not works

--- sbgECom/examples/ellipseMinimal/src/MainLoop.cpp
+++ sbgECom/examples/ellipseMinimal/src/MainLoop.cpp
@@ -1,30 +1,40 @@
 #include "Rocket.h"
 #include "chrono"
 #include "iostream"
+#include <thread>
 
-#define DELAYMS 5000 //5000 microseconds = 0.005s = 200 Hz
+#define DELAYMS 1000000000 //5000 microseconds = 0.005s = 200 Hz
 
 
 int main()
 {
 	// create a rocket instance
-	Rocket uOttRocket;
+	//Rocket uOttRocket;
 	
+	std::cout << "ok";
 	std::chrono::time_point<std::chrono::high_resolution_clock> start, now; 
-	std::chrono::microseconds ms;
-	std::chrono::duration<long double> elapsed_seconds;
-	start = std::chrono::system_clock::now();
-
+	start = std::chrono::high_resolution_clock::now();
 	
+	unsigned long int count = 1;
 	while (true) {
+		// uOttRocket.updateRocket();
+		
 		now = std::chrono::high_resolution_clock::now();
-		elapsed_seconds = now - start;
-		ms = std::chrono::duration_cast<std::chrono::microseconds>(elapsed_seconds);
-		std::cout << ms.count() << "\n";
+		std::chrono::duration<int64_t, std::nano> elapsed_seconds = now - start;
+		
+		std::chrono::microseconds elapsed_seconds_ms = std::chrono::duration_cast<std::chrono::microseconds>(elapsed_seconds);
+		std::chrono::duration<int64_t, std::nano> target_ms(DELAYMS * count++);
+		//https://www.geeksforgeeks.org/chrono-in-c/
+		
+		
+		std::cout << elapsed_seconds.count() << " : ";
+		// std::cout << ms.count() << "\n";
 
+		std::this_thread::sleep_for(target_ms - elapsed_seconds_ms);
 
-
-		//uOttRocket.updateRocket();
+		// std::this_thread::sleep( boost::posix_time::milliseconds(1000) );
+		
+		
 	}
 
 
