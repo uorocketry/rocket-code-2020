c5a958b8ada654009f4d32ec1f93b2f1a7138121
Sebastien Girard <sebastiengrd@gmail.com>
2020-08-24 00:23:24 -0400

    NOT WORKING Worked on exception handling
    Co-authored-by: Zachary Legros <ZachLegros@users.noreply.github.com>

--- groundStationSoftware/socket/client.cpp
+++ groundStationSoftware/socket/client.cpp
@@ -5,45 +5,59 @@
 #include <unistd.h> 
 #include <string.h> 
 #include <iostream>
+#include <thread>
+#include "chrono"
 #define PORT 8080
    
 int main(int argc, char const *argv[]) 
 { 
-    int sock = 0, valread; 
-    struct sockaddr_in serv_addr; 
-    char *hello = "Hello from client"; 
-    char buffer[1024] = {0}; 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-    { 
-        printf("\n Socket creation error \n"); 
-        return -1; 
-    } 
-   
-    serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(PORT); 
-       
-    // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
-    { 
-        printf("\nInvalid address/ Address not supported \n"); 
-        return -1; 
-    } 
    
     
     while (1)
     {
+        std::this_thread::sleep_for(std::chrono::duration<int64_t, std::nano>(1000000000));
+        int sock = 0, valread; 
+        struct sockaddr_in serv_addr;
+        if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+        { 
+            printf("\n Socket creation error \n"); 
+            continue;
+        } 
+    
+        serv_addr.sin_family = AF_INET; 
+        serv_addr.sin_port = htons(PORT); 
+        
+        // Convert IPv4 and IPv6 addresses from text to binary form 
+        if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
+        { 
+            printf("\nInvalid address/ Address not supported \n"); 
+            continue;
+        } 
+        std::cout << "Try to connect\n";
         if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == 0) 
         { 
-            break;
-        } 
-    }
+            char buffer[1] = {-1}; 
+            // send(sock , hello , strlen(hello) , 0 ); 
+            // printf("Hello message sent\n"); 
+            bool connected = true;
+            while(connected) 
+            {
+                std::cout << "Before " << (int)buffer[0] << " ";
+                valread = read( sock , buffer, 1);
+                // std::cout << valread << "\n";
 
-    // send(sock , hello , strlen(hello) , 0 ); 
-    // printf("Hello message sent\n"); 
-    while(true) {
-        valread = read( sock , buffer, 1);
-        std::cout << (int)buffer[0] << "\n"; 
-        // buffer = {0};
-    }
+                if(valread == 0) {
+                    connected = false;
+                } else {
+                    std::cout << (int)buffer[0] << "\n"; 
+                    // buffer[0] = -1;
+                }
+                std::this_thread::sleep_for(std::chrono::duration<int64_t, std::nano>(100000000));
+            }
+        } 
+        
+    }   
+
+  
     return 0; 
 } 
--- groundStationSoftware/socket/pyServer.py
+++ groundStationSoftware/socket/pyServer.py
@@ -1,5 +1,38 @@
 import socket
-import serial          
+import serial
+import time   
+import sys
+import glob
+
+
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result 
+
 #from time import sleep
 
 #ttyACM0
@@ -17,7 +50,7 @@ port = 8080
 # this makes the server listen to requests  
 # coming from other computers on the network 
 
-# s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 s.bind(('127.0.0.1', port))         
 print("socket binded to %s" %(port))
@@ -27,27 +60,49 @@ s.listen(5)
 print("socket is listening")
 
 
+
 # ser.write(b'hello')     # write a string
-  
+
 # a forever loop until we interrupt it or  
 # an error occurs 
+def openSerial():
+    global ser
+    valid = False
+    while(not valid):
+        time.sleep(1)
+        try:
+            ser = serial.Serial(serial_ports()[0], 9600, timeout=1)
+            ser.flush()
+            valid = True
+        except:
+            valid = False
+            
 
-with serial.Serial('/dev/ttyACM0', 9600, timeout=1) as ser:
-    # s = ser.read(1)        # read up to ten bytes (timeout)
-    # line = ser.readline()   # read a '\n' terminated line
+openSerial()
+# s = ser.read(1)        # read up to ten bytes (timeout)
+# line = ser.readline()   # read a '\n' terminated line
 
-    while True:   
-        # Establish connection with client. 
-        c, addr = s.accept()
-        with c:
-            print('Connected by', addr)
-            while True:
-                # data = c.recv(1024)
-                # print("[RECEIVED] " + data.decode("utf-8"));
-                
+while True:   
+    # Establish connection with client. 
+    c, addr = s.accept()
+    with c:
+        print('Connected by', addr)
+        while True:
+            # data = c.recv(1024)
+            # print("[RECEIVED] " + data.decode("utf-8"));
+            try:
                 x = ser.read()          # read one byte
-                #x = b'1'
-                # read serial
-                # if not data: break
+            except:
+                openSerial()
+                break
+
+            try:
                 c.send(x)
-                #sleep(1)
+            except:
+                break
+            
+            #x = b'1'
+            # read serial
+            # if not data: break
+                
+            #sleep(1)
