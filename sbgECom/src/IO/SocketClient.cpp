da62c4adfce0bfd68c2bf07ffc0dee297a8a00c0
zachlegros <legroszach3921@gmail.com>
2020-08-27 23:36:33 -0400

    Integrated tcp socket client in SocketClient.cpp
    Co-authored-by: Sebastien Girard <sebastiengrd@users.noreply.github.com>

--- sbgECom/src/IO/SocketClient.cpp
+++ sbgECom/src/IO/SocketClient.cpp
@@ -1,11 +1,20 @@
 #ifndef TESTING
 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
 #include "IO.h"
 #include "../data/SBGData.h"
 #include <iostream>
 #include"SocketClient.h"
 #include "chrono"
 #include <thread>
+
+#define PORT 8080
+
+
 
 SocketClient::SocketClient() {
 
@@ -17,8 +26,45 @@ SocketClient::~SocketClient() {
 
 void SocketClient::run() {
     while (true) {
-        // std::cout << "running client\n";
-        // std::this_thread::sleep_for(std::chrono::duration<int64_t, std::nano>(1000000000));
+        std::this_thread::sleep_for(std::chrono::duration<int64_t, std::nano>(1000000000));
+        int sock = 0, valread; 
+        struct sockaddr_in serv_addr;
+        if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+        { 
+            printf("\n Socket creation error \n"); 
+            continue;
+        } 
+    
+        serv_addr.sin_family = AF_INET; 
+        serv_addr.sin_port = htons(PORT); 
+        
+        // Convert IPv4 and IPv6 addresses from text to binary form 
+        if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
+        { 
+            printf("\nInvalid address/ Address not supported \n"); 
+            continue;
+        } 
+        std::cout << "Trying to connect...\n";
+        if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == 0) 
+        { 
+            std::cout << "Connected\n";
+            char buffer[1] = {-1}; 
+            bool connected = true;
+            while(connected) 
+            {
+                valread = read( sock , buffer, 1);
+
+                if(valread == 0) {
+                    connected = false;
+                    close(sock);
+                } 
+
+                std::cout << (int)buffer[0] << "\n";
+                buffer[0] = -1;
+
+                std::this_thread::sleep_for(std::chrono::duration<int64_t, std::nano>(100000000));
+            }
+        } 
     }
 
 }
