75b125b1ffde6196c6e733b3fb0f6f6abe430bd1
Ajay Ramachandran <dev@ajay.app>
2020-10-10 14:27:29 -0400

    Added environment variable to switch to run HotFire test

--- sbgECom/src/data/RocketSMData.h
+++ sbgECom/src/data/RocketSMData.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include "helpers/Types.h"
+#include "UOSMData.h"
 
-#include "stateMachineLib/StateMachine.h"
+#include "../helpers/Types.h"
+#include "../stateMachineLib/StateMachine.h"
 
-struct RocketSMData : public EventData {
-    /** Keep in mind, this is NOT the time since unix epoch (1970), and not the system time */
-	time_point now;
+struct RocketSMData : public UOSMData {
+
 };
--- sbgECom/src/init/MainLoop.cpp
+++ sbgECom/src/init/MainLoop.cpp
@@ -1,6 +1,12 @@
 #include "../stateMachine/Rocket.h"
 #include "../helpers/Types.h"
 
+#ifdef HOTFIRE_TEST
+#include "../stateMachine/HotFire.h"
+#else
+#include "../stateMachine/Rocket.h"
+#endif
+
 #include <stdio.h>
 #include "chrono"
 #include "iostream"
@@ -9,8 +15,12 @@
 #define TARGET_DELAY_NS 1000000000L/200L // in nanoseconds = 5 miliseconds = 200Hz
 
 int main() {
-	// create a rocket instance
-	Rocket uOttRocket;
+	// create a state machine instance
+#ifdef HOTFIRE_TEST
+	HotFire uOttSM;
+#else
+	Rocket uOttSM;
+#endif
 
 	time_point start, now; 
 	duration_ns target_ns, elapsed_ns;
@@ -21,10 +31,14 @@ int main() {
 		// Keep in mind, this is NOT the time since unix epoch (1970), and not the system time
 		now = std::chrono::steady_clock::now();
 
-		RocketSMData data;
+#ifdef HOTFIRE_TEST
+		UOSMData data = HotFireSMData();
+#else
+		UOSMData data = RocketSMData();
+#endif
 		data.now = now;
 
-		uOttRocket.updateStateMachine(&data);
+		uOttSM.updateStateMachine(&data);
 
 		elapsed_ns = duration_ns(now - start);
 		target_ns = duration_ns(TARGET_DELAY_NS * count++);		
