edd13e6f8aa67121abeea5efa89742a95db675c5
Ajay Ramachandran <dev@ajay.app>
2021-01-23 18:59:57 -0500

    Increase wait time to ensure it has written to disk
    Also add file name to error message

--- tests/octoberSky/run_test.sh
+++ tests/octoberSky/run_test.sh
@@ -5,7 +5,7 @@ mkdir -p output
 TEST_PATH=/tests/octoberSky/
 
 export TESTING=1
-export TARGET_UPDATE_DURATION_NS=0
+export TARGET_UPDATE_DURATION_NS=1000000
 export LOG_PATH="../..${TEST_PATH}output/"
 export TESTING_INPUT_FILE="../..${TEST_PATH}input.txt"
 
@@ -14,12 +14,16 @@ cd ../../
 
 cd .${TEST_PATH}
 
+FAIL=0
 for file in ./output-expected/*; do
-    diff "$file" "./output/${file##*/}" || FAIL=1
+    if ! diff "$file" "./output/${file##*/}"; then
+        FAIL=1
+        printf "\nError in the following file: ${file##*/}\n\n"
+    fi
 done
 
 rm -rf output
 
-if [ $FAIL = "1" ]; then
+if [[ $FAIL = "1" ]]; then
     exit 1
 fi
