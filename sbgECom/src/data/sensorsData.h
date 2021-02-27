1d0f96d1a93d8140fc3d1979ba04bb1180280452
Roman Koval <romankoval10@gmail.com>
2021-02-27 14:13:04 -0500

    remove old events from sensorsData struct

--- sbgECom/src/data/sensorsData.h
+++ sbgECom/src/data/sensorsData.h
@@ -13,12 +13,4 @@ struct sensorsData
 #if USE_SBG
     sbgData sbg;
 #endif
-
-#if USE_INPUT
-    int inputEventNumber = -1;
-#endif
-
-#if USE_SOCKET_CLIENT
-    int clientEventNumber = -1;
-#endif
 };
