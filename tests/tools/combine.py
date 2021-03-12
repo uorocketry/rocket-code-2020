52d47f64658077f7220f9bb553e8f6ad835b7584
Sébastien Girard <sebastiengrd@gmail.com>
2021-03-12 00:04:23 -0500

    fixed tool. Now the generated file is in good order

--- tests/tools/combine.py
+++ tests/tools/combine.py
@@ -1,17 +1,23 @@
 from os import listdir
 from os.path import isfile, join
+import glob
 
-mypath = "./output/"
+mypath = "./data/"
+begining = "2"
 
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
-allText = ""
+files = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and f[0]=="2")]
 
-for f in sorted(listdir(mypath), key=len):
+allText = b""
+
+for f in sorted(files, key=lambda name: int(name.split(".")[1])):
+    #print(f)
     if isfile(join(mypath, f)):
-        with open("./output/" + f, 'r') as file:
+        with open(mypath + f, 'rb') as file:
             allText += file.read()
 
-file = open("result.txt", 'w')
+
+
+file = open("result.txt", 'wb')
 file.write(allText)
 file.close()
