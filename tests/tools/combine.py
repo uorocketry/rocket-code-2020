add631b82312406ef39d5695e8e950167f49b5e9
Ajay Ramachandran <dev@ajay.app>
2021-03-12 23:11:18 -0500

    Fix combine script

--- tests/tools/combine.py
+++ tests/tools/combine.py
@@ -8,16 +8,25 @@ begining = "2"
 
 files = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and f[0]=="2")]
 
-allText = b""
+allText = ""
 
 for f in sorted(files, key=lambda name: int(name.split(".")[1])):
-    #print(f)
     if isfile(join(mypath, f)):
-        with open(mypath + f, 'rb') as file:
-            allText += file.read()
+        with open(mypath + f, 'r') as file:
+            current = file.read()
+            
+            allText += current
+
+lines = allText.split("\n")
+finalText = ""
+for i, line in enumerate(lines):
+    if (line.startswith(",")):
+        finalText = finalText[:-1]
+    finalText += line + "\n"
+    if (i % 10000 == 0):
+        print(i, len(lines))
 
 
-
-file = open("result.txt", 'wb')
-file.write(allText)
+file = open("result.txt", 'w')
+file.write(finalText)
 file.close()
