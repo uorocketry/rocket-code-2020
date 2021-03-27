5ac49c5cd5ab32ad3e926e41123a60ddeb1679a2
Roman Koval <romankoval10@gmail.com>
2021-03-27 17:42:25 -0400

    fixed input file

--- tests/octoberSky/fix-input.py
+++ tests/octoberSky/fix-input.py
@@ -1,29 +1,27 @@
 with open("fixed.txt", "w") as output_file:
-
     with open("input.txt") as input_file:
         cols = 0
-        read = False
 
         while True:
             vals = input_file.read(1)
-            
+
             if not vals:
                 break
 
-            val = vals[0]
+            if cols == 24:
+                if vals == ",":
+                    output_file.write("0,")
+                else:
+                    s = bytes(vals, encoding='ascii')
+                    i = int.from_bytes(s, "little")
+                    output_file.write(str(i))
 
-            if cols == 24 and not read:
-                s = bytes(vals, encoding='ascii')
-                i = int.from_bytes(s, "little")
-                output_file.write(str(i))
-                read = True
+                cols += 1
                 continue
 
-            output_file.write(val)
+            output_file.write(vals)
 
-            if val == ",":
+            if vals == ",":
                 cols += 1
-            elif val == "\n":
+            elif vals == "\n":
                 cols = 0
-
-            read = False
