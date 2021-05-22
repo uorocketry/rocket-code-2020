76508a028c54d27ad82a4c22236a8571a390ea9e
Ajay Ramachandran <dev@ajay.app>
2021-05-22 18:12:19 -0400

    Lower minimum boost version and use latest when building from source

--- build-boost-arm.sh
+++ build-boost-arm.sh
@@ -10,7 +10,7 @@ if [ -z "$RPI_TOOLS" ]; then
   exit 1
 fi
 
-BOOST_VERSION=1.67.0
+BOOST_VERSION=1.76.0
 BOOST_DIRECTORY="boost_${BOOST_VERSION//\./_}"
 
 curl -fLO "https://sourceforge.net/projects/boost/files/boost/$BOOST_VERSION/$BOOST_DIRECTORY.tar.bz2"
