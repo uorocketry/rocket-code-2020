b90897dee2c854d0fb90d16ef17b1877a6702144
Jonathan Papineau <public@jpapineau.ca>
2021-07-30 16:11:37 -0400

    Adapt SPI wrapper for our use

--- src/IO/SPI.cpp
+++ src/IO/SPI.cpp
@@ -11,64 +11,58 @@
 #include <linux/spi/spidev.h>
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
-static void pabort(const char *s)
-{
-	perror(s);
-	abort();
-}
-
-SPI::SPI(const char* device, uint8_t mode, uint8_t bits, uint32_t speed, uint16_t delay) : device(device), mode(mode), bits(bits), speed(speed), delay(delay)
+SPI::SPI(const char* device, uint8_t mode, uint8_t bits, uint32_t speed, uint16_t delay) : bits(bits), speed(speed), delay(delay)
 {
 	int ret = 0;
 	
 	fd = open(device, O_RDWR);
-	if (fd < 0)
-		pabort("can't open device");
+	if (fd < 0) {
+        throw SPIError("Can't open device" + std::string(device));
+    }
 
 	/*
 	 * spi mode
 	 */
 	ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
-	if (ret == -1)
-		pabort("can't set spi mode");
+	if (ret == -1) {
+		throw SPIError("Can't set spi write mode to " + std::to_string(mode));
+	}
 
 	ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
-	if (ret == -1)
-		pabort("can't get spi mode");
+	if (ret == -1) {
+        throw SPIError("Can't set spi read mode to " + std::to_string(mode));
+    }
 
 	/*
 	 * bits per word
 	 */
 	ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
-	if (ret == -1)
-		pabort("can't set bits per word");
+	if (ret == -1) {
+        throw SPIError("Can't set read bits per word tp " + std::to_string(bits));
+    }
 
 	ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
-	if (ret == -1)
-		pabort("can't get bits per word");
+    if (ret == -1) {
+        throw SPIError("Can't set write bits per word tp " + std::to_string(bits));
+    }
 
 	/*
 	 * max speed hz
 	 */
 	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
-	if (ret == -1)
-		pabort("can't set max speed hz");
+	if (ret == -1) {
+        throw SPIError("Can't set max write speed hz to " + std::to_string(speed));
+    }
 
 	ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
-	if (ret == -1)
-		pabort("can't get max speed hz");
-
-	printf("\nspi mode: %d\n", mode);
-	printf("bits per word: %d\n", bits);
-	printf("max speed: %d Hz (%d KHz)\n", speed, speed/1000);
+    if (ret == -1) {
+        throw SPIError("Can't set max read speed hz to " + std::to_string(speed));
+    }
 }
 
-void SPI::printBytesBuffer(uint8_t buffer[], int len)
+SPI::~SPI()
 {
-	for (int i = 0; i < len; i++) {
-		std::cout << (int)buffer[i] << " ";
-	}
-	std::cout << std::endl;
+    close(fd);
 }
 
 void SPI::transfer(uint8_t tx[], uint8_t rx[], unsigned int len)
@@ -85,13 +79,7 @@ void SPI::transfer(uint8_t tx[], uint8_t rx[], unsigned int len)
 	};
 
 	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-	if (ret < 1)
-		pabort("can't send spi message");
-		puts("");
-	
-}
-
-void SPI::close_SPI()
-{
-	close(fd);
+	if (ret < 1) {
+        throw SPIError("can't send spi message");
+    }
 }
