40088104511cb2c60b9fe8997751fb3ffe021cba
Ajay <dev@ajay.app>
2022-01-08 19:58:02 -0500

    Add dependencies using pacman

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,6 +4,9 @@ if command -v apt >/dev/null; then
   # For apt-based distros (ex: Debian, Ubuntu, etc.)
   apt update
   apt install ninja-build cmake gcc g++ make libi2c-dev libboost-all-dev protobuf-compiler -y
+elif command -v pacman >/dev/null; then
+  # Arch based
+  pacman -S ninja cmake gcc boost boost-libs make i2c-tools protobuf
 else
   echo "Your distro doesn't seem to be supported."
 fi
