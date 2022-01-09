5c37abdb14bf2b88e10035fe7a03796175be650a
Jonathan Papineau <dev@jontech.app>
2022-01-08 20:18:13 -0500

    Add support for openSUSE in `install-dependencies.sh`

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -7,6 +7,9 @@ if command -v apt >/dev/null; then
 elif command -v pacman >/dev/null; then
   # Arch based
   pacman -S ninja cmake gcc boost boost-libs make i2c-tools protobuf systemd-libs
+elif command -v zypper >/dev/null; then
+  # openSUSE
+  zypper install ninja cmake-full gcc boost-devel make libi2c0-devel protobuf-devel systemd-devel
 else
   echo "Your distro doesn't seem to be supported."
 fi
