881ebb0a4af50e8ea2ab6b52172a8b8aaa5b744d
Jonathan Papineau <dev@jontech.app>
2022-03-02 22:13:00 -0500

    feat: create write() and write_then_read() SPI functions

--- src/IO/SPI.cpp
+++ src/IO/SPI.cpp
@@ -1,61 +1,86 @@
 #include "SPI.h"
-#include <stdint.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <getopt.h>
-#include <iostream>
+#include <cstdint>
+#include <cstring>
 #include <fcntl.h>
 #include <sys/ioctl.h>
-#include <linux/types.h>
-#include <linux/spi/spidev.h>
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+#include <unistd.h>
 
-SPI::SPI(const char* device, uint8_t mode, uint8_t bits, uint32_t speed, uint16_t delay) : bits(bits), speed(speed), delay(delay)
+/**
+ *
+ * @param device Path to the SPI device
+ * @param mode SPI mode. Use the constants SPI_MODE_0..SPI_MODE_3
+ * @param bitsPerWord Number of bits contained per spi word. Usually set to 8
+ * @param speed SPI speed in Hz
+ * @param lsbFirst Whether the LSB should be first or the MSB
+ */
+SPI::SPI(const char *device, uint8_t mode, uint8_t bitsPerWord, uint32_t speed, bool lsbFirst)
+    : device(device), bits(bitsPerWord), speed(speed)
 {
-	int ret = 0;
-	
-	fd = open(device, O_RDWR);
-	if (fd < 0) {
+    int ret = 0;
+
+    fd = open(device, O_RDWR);
+    if (fd < 0)
+    {
         throw SPIError("Can't open device" + std::string(device));
     }
 
-	/*
-	 * spi mode
-	 */
-	ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
-	if (ret == -1) {
-		throw SPIError("Can't set spi write mode to " + std::to_string(mode));
-	}
+    /*
+     * spi mode
+     */
+    ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
+    if (ret == -1)
+    {
+        throw SPIError("Can't set spi write mode to " + std::to_string(mode));
+    }
 
-	ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
-	if (ret == -1) {
+    ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
+    if (ret == -1)
+    {
         throw SPIError("Can't set spi read mode to " + std::to_string(mode));
     }
 
-	/*
-	 * bits per word
-	 */
-	ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
-	if (ret == -1) {
-        throw SPIError("Can't set read bits per word tp " + std::to_string(bits));
+    /*
+     * lsb
+     */
+    ret = ioctl(fd, SPI_IOC_WR_LSB_FIRST, &lsbFirst);
+    if (ret == -1)
+    {
+        throw SPIError("Can't set spi write lsb to " + std::to_string(lsbFirst));
     }
 
-	ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
-    if (ret == -1) {
-        throw SPIError("Can't set write bits per word tp " + std::to_string(bits));
+    ret = ioctl(fd, SPI_IOC_RD_LSB_FIRST, &lsbFirst);
+    if (ret == -1)
+    {
+        throw SPIError("Can't set spi write lsb to " + std::to_string(lsbFirst));
     }
 
-	/*
-	 * max speed hz
-	 */
-	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
-	if (ret == -1) {
+    /*
+     * bits per word
+     */
+    ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bitsPerWord);
+    if (ret == -1)
+    {
+        throw SPIError("Can't set read bits per word tp " + std::to_string(bitsPerWord));
+    }
+
+    ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bitsPerWord);
+    if (ret == -1)
+    {
+        throw SPIError("Can't set write bits per word tp " + std::to_string(bitsPerWord));
+    }
+
+    /*
+     * max speed hz
+     */
+    ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+    if (ret == -1)
+    {
         throw SPIError("Can't set max write speed hz to " + std::to_string(speed));
     }
 
-	ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
-    if (ret == -1) {
+    ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
+    if (ret == -1)
+    {
         throw SPIError("Can't set max read speed hz to " + std::to_string(speed));
     }
 }
@@ -65,21 +90,54 @@ SPI::~SPI()
     close(fd);
 }
 
-void SPI::transfer(uint8_t tx[], uint8_t rx[], unsigned int len)
+/*!
+ *    @brief  Write a buffer or two to the SPI device.
+ *    @param  buffer Pointer to buffer of data to write
+ *    @param  len Number of bytes from buffer to write
+ *    @param  prefix_buffer Pointer to optional array of data to write before
+ * buffer.
+ *    @param  prefix_len Number of bytes from prefix buffer to write
+ *    @return
+ */
+void SPI::write(uint8_t *buffer, size_t len) const
 {
-	int ret;
-	
-	struct spi_ioc_transfer tr = {
-		.tx_buf = (unsigned long)tx,
-		.rx_buf = (unsigned long)rx,
-		.len = len,
-		.speed_hz = speed,
-		.delay_usecs = delay,
-		.bits_per_word = bits,
-	};
+    auto status = ::write(fd, buffer, len);
+    if (status < 0)
+    {
+        throw SPIError("Error writing to SPI device");
+    }
+}
 
-	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-	if (ret < 1) {
-        throw SPIError("can't send spi message");
+/*!
+ *    @brief  Write some data, then read some data from SPI into another buffer.
+ * The buffers can point to same/overlapping locations. This does not
+ * transmit-receive at the same time!
+ *    @param  write_buffer Pointer to buffer of data to write from
+ *    @param  write_len Number of bytes from buffer to write.
+ *    @param  read_buffer Pointer to buffer of data to read into.
+ *    @param  read_len Number of bytes from buffer to read.
+ *    @param  sendvalue The 8-bits of data to write when doing the data read,
+ * defaults to 0xFF
+ *    @return Always returns true because there's no way to test success of SPI
+ * writes
+ */
+void SPI::write_then_read(const uint8_t *write_buffer, size_t write_len, uint8_t *read_buffer, size_t read_len,
+                          uint8_t sendvalue) const
+{
+    struct spi_ioc_transfer xfer[2];
+
+    memset(xfer, 0, sizeof xfer);
+    memset(read_buffer, 0, read_len);
+
+    xfer[0].tx_buf = (unsigned long)write_buffer;
+    xfer[0].len = write_len;
+
+    xfer[1].rx_buf = (unsigned long)read_buffer;
+    xfer[1].len = read_len;
+
+    int status = ioctl(fd, SPI_IOC_MESSAGE(2), xfer);
+    if (status < 0)
+    {
+        throw SPIError("Error reading to SPI device");
     }
 }
--- src/IO/SPI.h
+++ src/IO/SPI.h
@@ -1,27 +1,33 @@
 #ifndef SPI_H
 #define SPI_H
-#include <stdint.h>
+#include <cstdint>
+#include <linux/spi/spidev.h>
+#include <linux/types.h>
 #include <stdexcept>
 
 class SPIError : public std::runtime_error
 {
-public:
-    explicit SPIError(const std::string &str) : std::runtime_error(str) {}
+  public:
+    explicit SPIError(const std::string &str) : std::runtime_error(str)
+    {
+    }
 };
 
 class SPI
 {
   private:
     int fd;
-    const char* device;
+    const char *device;
     uint8_t bits;
     uint32_t speed;
-    uint16_t delay;
 
   public:
-    SPI(const char* device, uint8_t mode, uint8_t bits, uint32_t speed, uint16_t delay);
+    SPI(const char *device, uint8_t mode, uint8_t bitsPerWord, uint32_t speed, bool lsbFirst);
     ~SPI();
-    void transfer(uint8_t tx[], uint8_t rx[], unsigned int len);
+
+    void write(uint8_t *buffer, size_t len) const;
+    void write_then_read(const uint8_t *write_buffer, size_t write_len, uint8_t *read_buffer, size_t read_len,
+                         uint8_t sendvalue = 0xFF) const;
 };
 
 #endif
