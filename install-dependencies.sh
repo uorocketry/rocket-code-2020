ebf63579c0c964f733bbc9c626da7f79a04c8af7
Jonathan Papineau <dev@jontech.app>
2022-03-07 20:22:41 -0500

    feat: install and link to ZeroMQ

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,13 +3,13 @@
 if command -v apt >/dev/null; then
   # For apt-based distros (ex: Debian, Ubuntu, etc.)
   apt update
-  apt install ninja-build cmake gcc g++ make libi2c-dev libboost-all-dev protobuf-compiler libsystemd-dev -y
+  apt install -y ninja-build cmake gcc g++ make libi2c-dev libboost-all-dev protobuf-compiler libsystemd-dev libzmq3-dev
 elif command -v pacman >/dev/null; then
   # Arch based
-  pacman -S ninja cmake gcc boost boost-libs make i2c-tools protobuf systemd-libs
+  pacman -S ninja cmake gcc boost boost-libs make i2c-tools protobuf systemd-libs zeromq
 elif command -v zypper >/dev/null; then
   # openSUSE
-  zypper install ninja cmake-full gcc boost-devel make libi2c0-devel protobuf-devel systemd-devel
+  zypper install ninja cmake-full gcc boost-devel make libi2c0-devel protobuf-devel systemd-devel cppzmq-devel
 else
   echo "Your distro doesn't seem to be supported."
 fi
