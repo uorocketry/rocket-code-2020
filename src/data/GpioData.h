cd5c99f1f9cae3ec27dc69735d496430dd9bb6c7
Fernando Nogueira <abert036@uottawa.ca>
2022-03-12 05:16:27 -0500

    refactor: simplify GpioData and GpioState

--- src/data/GpioData.h
+++ src/data/GpioData.h
@@ -1,15 +1,7 @@
 #pragma once
-#include "config.h"
-#include <map>
-#include <string>
+
 struct GpioData
 {
-    std::map<std::string, int> digitalOutputMap;
-    std::map<std::string, int> pwmOutputMap;
-};
-
-struct GpioState
-{
-    std::map<std::string, int> digitalStateMap;
-    std::map<std::string, int> pwmStateMap;
+    std::map<std::string, int> digitalMap;
+    std::map<std::string, int> pwmMap;
 };
