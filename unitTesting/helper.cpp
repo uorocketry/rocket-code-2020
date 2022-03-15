7b4a431074880bf43ebb452457a96135cc16aaa0
Fernando Nogueira <abert036@uottawa.ca>
2022-03-15 17:40:53 -0400

    refactor: replace stringSplit by boost::split

--- unitTesting/helper.cpp
+++
@@ -1,7 +0,0 @@
-#include <catch2/catch.hpp>
-#include <helpers/Helper.h>
-
-TEST_CASE("Helper String Split", "[helper]")
-{
-    REQUIRE(helper::stringSplit("item1,item2,item3", ',') == std::vector<std::string>{"item1", "item2", "item3"});
-}
