a89a6e32d8e8b14a0b648aa8461f408756fe835c
Ajay <dev@ajay.app>
2022-08-05 06:25:39 -0400

    Lower partial burn to two seconds

--- src/stateMachine/HotFire/HotFireStateMachine.cpp
+++ src/stateMachine/HotFire/HotFireStateMachine.cpp
@@ -563,7 +563,7 @@ STATE_DEFINE(HotFireStateMachine, IgnitionBurn, UOSMData)
 #endif
     EventData eventData;
     detectExternEvent(interfaceData);
-    switchStatesAfterTime((ST_FULL_BURN), duration_ms(5000), eventData);
+    switchStatesAfterTime((ST_FULL_BURN), duration_ms(2000), eventData);
 
     interface->updateOutputs(interfaceData);
 }
